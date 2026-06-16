32a4227681a2bb2b3000cef62fb3d46507f6dce1
Deekshitha-123-code <Sdeekshithasenthil@gmail.com>
2026-06-16 12:30:34 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,6 @@
+num1=float(input("enter first number: "))
+num2=float(input("enter second number:"))
+print("add:",num1+num2)
+print("sub:",num1-num2)
+print("mul:",num1*num2)
+print("div:",num1/num2)
